91ee36b1b54a92b01c0a32e64329b88265d19a11
reemo <r48n34@gmail.com>
2023-11-07 19:01:40 +0800

    Add SEO stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,29 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ registerType: 'autoUpdate' })
-  ],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico'],
+            manifest: {
+                name: 'Modern Table Tennis Score Board',
+                short_name: 'TT Score Board',
+                description: 'Nice Modern Table Tennis Score Board',
+                theme_color: '#ffffff',
+                icons: [
+                {
+                    src: 'favicon.ico',
+                    sizes: '192x192',
+                    type: 'image/ico'
+                },
+                {
+                    src: 'favicon.ico',
+                    sizes: '512x512',
+                    type: 'image/ico'
+                }
+                ]
+            }
+        })
+    ],
 })
