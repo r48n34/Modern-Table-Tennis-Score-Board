5db882d8eafc2a1ba5ae136f7ddc2ebde5ff12da
reemo <r48n34@gmail.com>
2025-06-05 12:25:04 +0800

    Update BigScoreMultiPages.tsx

--- src/pages/BigScoreMultiPages.tsx
+++ src/pages/BigScoreMultiPages.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mantine/core";
+import { Card, Container, Grid, Space } from "@mantine/core";
 import BigScoreBoard from "../components/BigScoreBoard";
 import { Helmet } from "react-helmet-async";
 
@@ -9,22 +9,24 @@ function BigScoreMultiPages() {
                 <title>Multi Score Board | Modern Table Tennis Score Board</title>
             </Helmet>
 
-            <Grid>
+            <Space h="lg" />
 
-                <Grid.Col span={{ base: 12, md: 12, lg: 6 }}>
-                    <BigScoreBoard showTitle={false} showsColorTheme={false} showRoadmap={false} uid="aa" />
-                </Grid.Col>
-                <Grid.Col span={{ base: 12, md: 12, lg: 6 }}>
-                    <BigScoreBoard showTitle={false} showsColorTheme={false} showRoadmap={false} uid="bb" />
-                </Grid.Col>
-                <Grid.Col span={{ base: 12, md: 12, lg: 6 }}>
-                    <BigScoreBoard showTitle={false} showsColorTheme={false} showRoadmap={false} uid="cc" />
-                </Grid.Col>
-                <Grid.Col span={{ base: 12, md: 12, lg: 6 }}>
-                    <BigScoreBoard showTitle={false} showsColorTheme={false} showRoadmap={false} uid="dd" />
-                </Grid.Col>
-
-            </Grid>
+            <Container fluid>
+                <Grid>
+                    {["aa", "bb", "cc", "dd"].map(v => (
+                        <Grid.Col span={{ base: 12, md: 12, lg: 6 }}>
+                            <Card shadow="sm" padding="lg" radius="md" withBorder>
+                                <BigScoreBoard
+                                    showTitle={false}
+                                    showsColorTheme={false}
+                                    showRoadmap={false}
+                                    uid={v}
+                                />
+                            </Card>
+                        </Grid.Col>
+                    ))}
+                </Grid>
+            </Container>
         </>
     )
 }
